efa0bb0af9000e010ab8268d4323e5b04a3464ab
JavaNewBeer <2696587731@qq.com>
2018-12-12 09:40:33 +0800

    GitTest.... 添加UUid

---
+++ src/test/java/com/supply/demo/UUIDUtils.java
@@ -0,0 +1,101 @@
+package com.supply.demo;/**
+ * 作者: UUIDUtils-ljh
+ * 创建时间: 2018/12/12 9:22
+ * 声明:
+ */
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+/**
+ *@ClassName UUIDUtils
+ *@Author Junlunet-ljh
+ *@Date 2018/12/12 9:22
+ *@Version 1.0
+ **/
+public class UUIDUtils {
+    /**加密信息
+     * 一次编码      将在传入的map中添加AccessKeyId、Timestamp、Method、
+     * @param apiKey       AppKeyId.
+     * @param apiSecretKey AppKeySecret.
+     * @param urlPath      请求路径，注意不含?以及后的参数，例如"/v1/api/info"
+     */
+    public String baseDigest(String apiKey, String apiSecretKey,String urlPath) throws IOException, JSONException {
+
+        long nonce = System.currentTimeMillis();
+        //必传的2个加密参数
+        JSONObject jo = new JSONObject();
+        jo.put("request", urlPath);
+        jo.put("nonce", Long.toString(nonce));
+
+        // payload = parameters-object -> JSON encode -> base64
+        String payload = jo.toString();
+        String payload_base64 = Base64.getEncoder().encodeToString(payload.getBytes()); //Base64加密
+
+        return payload_base64;
+    }
+
+    /**创建签名
+     * 二次编码使payload_base64实现algo的哈希（HMAC SHA384）sha384加密
+     * @param msg           初步编码后的URL路径payload_base64
+     * @param keyString     AppKeySecret.
+     * @param algo          编码格式
+     * @return
+     */
+    public static String hmacDigest(String msg, String keyString, String algo) {
+        String digest = null;
+        try {
+            SecretKeySpec key = new SecretKeySpec((keyString).getBytes("UTF-8"), algo);
+            Mac mac = Mac.getInstance(algo);
+            mac.init(key);
+
+            byte[] bytes = mac.doFinal(msg.getBytes("ASCII"));
+
+            StringBuffer hash = new StringBuffer();
+            for (int i = 0; i < bytes.length; i++) {
+                String hex = Integer.toHexString(0xFF & bytes[i]);
+                if (hex.length() == 1) {
+                    hash.append('0');
+                }
+                hash.append(hex);
+            }
+            digest = hash.toString().substring(0,32);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return digest;
+    }
+
+    @Test
+    public void printUUID(){
+        String api = "bf";
+        String secret = "ljh";
+        String order = "eid";
+        String payload_base64 = "";
+        String payload_32_sha384hmac = "";
+        try {
+            payload_base64 = baseDigest(api,secret,order);
+            payload_32_sha384hmac = hmacDigest(payload_base64, secret, "HmacSHA384");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        System.out.println("加密后UUID1：" + payload_base64);
+        System.out.println("加密后UUID2：" + payload_32_sha384hmac);
+    }
+
+}
